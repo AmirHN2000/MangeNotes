40a0537c4dc6e1e4e909f937f73116fe9493c453
amir.H <amir.hnaeemian2000@gmail.com>
2021-09-04 13:02:09 +0430

    rebook edit account controller

--- ManageNotes/Controllers/AccountController.cs
+++ ManageNotes/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ namespace ManageNotes.Controllers
 {
     public class AccountController : Controller
     {
+        //get
         public IActionResult AccessDenied()
         {
             return View();
